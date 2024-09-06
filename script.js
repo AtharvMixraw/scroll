8fc5cf8fab47d919e0576bda0c821f26ef2afedf
Atharv Mishra <antilogatharv@gmail.com>
2024-09-07 03:38:12 +0530

    changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('scrollBox').addEventListener('scroll', function() {
     const message = document.getElementById('message');
-    // Check if user has scrolled to the bottom
-    if (this.scrollTop + this.clientHeight >= this.scrollHeight) {
+    // Display the message as soon as scrolling occurs
+    if (this.scrollTop > 0) {
         message.classList.remove('hidden');
         message.classList.add('visible');
     }
